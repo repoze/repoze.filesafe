b694f3ed93367e6a3873f213c7f2c6db76134ac8
Chris McDonough <chrism@plope.com>
2007-09-18 20:24:43 +0000

    Prep for 0.5.

--- setup.py
+++ setup.py
@@ -1,36 +1,34 @@
-__version__ = '0.4'
+__version__ = '0.5'
 
 import os
 from setuptools import setup, find_packages
 
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+
 setup(name='repoze.tm',
       version=__version__,
       description='Zope-like transaction manager via WSGI middleware',
-      long_description='',
+      long_description=README,
       classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: Zope Public License",
         "Programming Language :: Python",
         "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Internet :: WWW/HTTP :: WSGI",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
-        "Framework :: Zope3",
         ],
       keywords='web application server wsgi zope repoze',
       author="Agendaless Consulting",
       author_email="repoze-dev@lists.repoze.org",
       dependency_links=['http://dist.repoze.org'],
       url="http://www.repoze.org",
-      license="ZPL 2.0",
+      license="BSD-derived (http://www.dist.repoze.org/LICENSE.txt)",
       packages=find_packages(),
       include_package_data=True,
       namespace_packages=['repoze'],
       zip_safe=False,
-      install_requires=['zopelib >= 2.10.4.2'],
+      install_requires=['ZODB3 >= 3.7.1'],
       test_suite = "repoze.tm.tests",
       entry_points="""
       [paste.filter_app_factory]
