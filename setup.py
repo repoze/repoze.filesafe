020fc772b4e2450c574516d689fd8d64edbec153
Wichert Akkerman <wichert@wiggy.net>
2014-05-06 14:34:55 +0200

    Prepare 2.1 release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-version = '2.0'
+version = '2.1'
 
 setup(name='repoze.filesafe',
       version=version,
